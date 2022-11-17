e0198c223afdf3d23c861b4f496472ec22c2f0f2
Paul <skrimorill@mail.ru>
2022-11-17 05:52:40 +0300

    2-objects-and-arrays added

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,14 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const arr2 = [];
+  for (let i = 0; i < array.length; i++) {
+    arr2[i] = fn(array[i], i, array);
+    // arr2.push(array[i] ** 2);
+  }
+  return arr2;
+}
 
 /*
  Задание 3:
@@ -31,7 +42,16 @@ function map(array, fn) {}
  Пример:
    reduce([1, 2, 3], (all, current) => all + current) // 6
  */
-function reduce(array, fn, initial) {}
+function reduce(array, fn, initial) {
+  let ss = initial || array[0];
+  let i = initial ? 0 : 1;
+
+  for (; i < array.length; i++) {
+    ss = fn(ss, array[i], i, array);
+  }
+
+  return ss;
+}
 
 /*
  Задание 4:
@@ -41,12 +61,18 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const res = [];
+  for (const prop in obj) {
+    res.push(prop.toUpperCase());
+  }
+  return res;
+}
 
 /*
  Задание 5 *:
 
- Функция принимает объект и должна вернуть Proxy для этого объекта
+ 
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
 
  Пример:
